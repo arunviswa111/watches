9ec55b11669a48a11ccd896f393c2c5476f0112b
harshayvlsk <harshayvlsk@gmail.com>
2016-10-25 16:33:23 +0530

    this is my second commit

---
+++ Timef/src/main/java/com/niit/Timef/Controller/HomeController.java
@@ -0,0 +1,45 @@
+package com.niit.Timef.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String gotoHome()
+	{
+		return "index";
+	}
+	@RequestMapping("/validate")
+	public String validate(@RequestParam("Userid") String id, @RequestParam("Password") String pwd,Model model)
+	{
+		if(id.equals("arun") && pwd.equals("arun"))
+		{
+			model.addAttribute("success", "Loggedin");
+			return "index";
+		}
+		else
+		{
+			model.addAttribute("error", "invalid");
+			return "index";
+		}
+		
+	}
+
+	@RequestMapping("Login")
+	public String login(Model model)
+	{
+	model.addAttribute("userClickedLogin", "true");
+	return "index";
+	}
+	@RequestMapping("Register")
+	public String register(Model model)
+	{
+	model.addAttribute("userClickedRegister", "true");
+	return "index";
+	}
+	
+
+}
---
+++ Timef/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,36 @@
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib prefix="c" 
+           uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h2>  <center> Online Shopping  </center>  </h2>
+
+Existing user <a href="Login"> login here </a>
+New user <a href="Register"> register here </a>
+<hr>
+
+
+
+<c:if test="${userClickedLogin}">
+
+    <jsp:include page="login.jsp"></jsp:include>
+    
+</c:if>   
+<c:if test="${userClickedRegister}">
+
+    <jsp:include page="register.jsp"></jsp:include>
+    
+</c:if>  
+
+<c:out value="${success}"></c:out>
+<c:out value="${error}"></c:out> 
+
+</body>
+</html>
